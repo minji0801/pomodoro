083dc6df27eefe93056f43fcbb9193629c095792
Minji Kim <minji_0801@naver.com>
2021-10-26 12:56:24 +0900

    Initial Commit

---
+++ pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by 김민지 on 2021/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
